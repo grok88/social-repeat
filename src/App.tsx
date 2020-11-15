5bf242c9478ed7f04806b9dd6804724b6753b5ed
Alex <grok88@tut.by>
2020-11-15 20:25:39 +0300

    11 - grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                header
+            </header>
+            <nav className={'nav'}>
+                <div>
+                    <a href="#">Profile</a>
+                </div>
+                <div>
+                    <a href="#">Messages</a>
+                </div>
+                <div>
+                    <a href="#">News</a>
+                </div>
+                <div>
+                    <a href="#">Users</a>
+                </div>
+                <div>
+                    <a href="#">Setting</a>
+                </div>
+            </nav>
+            <div className={'content'}>
+               <div>
+                   <img src="https://www.neuroarchitecting.org/background.jpg" alt="main_image" width={'100%'} height={'300px'}/>
+               </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    my Posts
+                    <div>
+                        Post
+                    </div>
+                </div>
+                <div>
+                    messages
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
