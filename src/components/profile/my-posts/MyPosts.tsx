875bd968cf1cb8d033413c801b076cec5ce0fff5
Alex <grok88@tut.by>
2020-11-16 10:33:06 +0300

    16 - props

--- src/components/profile/my-posts/MyPosts.tsx
+++ src/components/profile/my-posts/MyPosts.tsx
@@ -10,9 +10,9 @@ const MyPosts = () => {
                 <textarea></textarea>
                 <button>add Post</button>
             </div>
-            <Post/>
-            <Post/>
-            <Post/>
+            <Post message={'This is Sparta'} like={5}/>
+            <Post message={'Wow, You are big'} like={6}/>
+            <Post message={'Wow, I am good'} like={7}/>
         </div>
     );
 };
--- src/components/profile/my-posts/Post/Post.tsx
+++ src/components/profile/my-posts/Post/Post.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from './Post.module.css'
 
-const Post = () => {
+type PostPropsType = {
+    message: string
+    like: number
+}
+const Post: React.FC<PostPropsType> = ({message, like}) => {
     return (
         <div className={styles.item}>
-            <img src="https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg" className={styles.img}/>
-            Post 1
-            <span>5 like</span>
+            <img
+                src="https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"
+                className={styles.img}/>
+            {message}
+            <div>
+                <span>{like} like</span>
+            </div>
         </div>
     );
 };
