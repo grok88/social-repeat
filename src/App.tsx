22f828ea6e1d0426502ac013ea870fc3df14d528
Alex <grok88@tut.by>
2020-11-16 09:08:38 +0300

    12

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+import Profile from "./components/profile/Profile";
 
 function App() {
     return (
         <div className={'app-wrapper'}>
-            <header className={'header'}>
-                header
-            </header>
-            <nav className={'nav'}>
-                <div>
-                    <a href="#">Profile</a>
-                </div>
-                <div>
-                    <a href="#">Messages</a>
-                </div>
-                <div>
-                    <a href="#">News</a>
-                </div>
-                <div>
-                    <a href="#">Users</a>
-                </div>
-                <div>
-                    <a href="#">Setting</a>
-                </div>
-            </nav>
-            <div className={'content'}>
-               <div>
-                   <img src="https://www.neuroarchitecting.org/background.jpg" alt="main_image" width={'100%'} height={'300px'}/>
-               </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    my Posts
-                    <div>
-                        Post
-                    </div>
-                </div>
-                <div>
-                    messages
-                </div>
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img src="https://www.neuroarchitecting.org/background.jpg" alt="main_image" width={'100%'} height={'300px'}/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my Posts
+                <div>
+                    Post
+                </div>
+            </div>
+            <div>
+                messages
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
