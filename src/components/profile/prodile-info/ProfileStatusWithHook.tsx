ba8618f371e3b6f2d6506ccbfd6e01058f92a06f
Alex <grok88@tut.by>
2020-12-03 16:32:26 +0300

    85 - useEffect Hook

--- src/components/profile/prodile-info/ProfileStatusWithHook.tsx
+++ src/components/profile/prodile-info/ProfileStatusWithHook.tsx
@@ -1,15 +1,17 @@
-import React, {ChangeEvent} from "react";
-import { useState } from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 
 
 type ProfileStatusType = {
     status: string
     updateStatus: (status: string) => void
 }
- export const ProfileStatusWithHook: React.FC<ProfileStatusType>  = (props) =>{
+export const ProfileStatusWithHook: React.FC<ProfileStatusType> = (props) => {
     const [editMode, setEditMode] = useState<boolean>(false);
     const [status, setStatus] = useState<string>(props.status);
 
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status]);
 
     const activateEditMode = () => {
         setEditMode(true);
@@ -17,30 +19,21 @@ type ProfileStatusType = {
 
     const deActivateEditMode = () => {
         setEditMode(false);
-       props.updateStatus(status);
+        props.updateStatus(status);
     }
-    // componentDidUpdate(prevProps: Readonly<ProfileStatusType>, prevState: Readonly<{}>, snapshot?: any) {
-    //     if(prevProps.status !== this.props.status){
-    //         this.setState({
-    //             status:this.props.status
-    //         })
-    //     }
-    // }
 
     const onChangeStatus = (e: ChangeEvent<HTMLInputElement>) => {
         setStatus(e.currentTarget.value);
     }
 
-
-        return <div>
-            <strong>Status: </strong>
-            {
-                editMode ? <input type="text" onChange={onChangeStatus} value={status}
-                                              onBlur={deActivateEditMode}/> : <span onClick={activateEditMode}>{props.status || 'no Status'}</span>
-            }
-        </div>
-
+    return <div>
+        <strong>Status: </strong>
+        {
+            editMode ? <input type="text" onChange={onChangeStatus} value={status}
+                              onBlur={deActivateEditMode}/> :
+                <span onClick={activateEditMode}>{props.status || 'no Status'}</span>
+        }
+    </div>
 }
-
 
 export default ProfileStatusWithHook;
